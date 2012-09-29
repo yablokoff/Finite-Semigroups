cc63ac9cc235327aac6246dbe08de078088d69ef
Dmitrii Vlasov <yablokoff.tlt@gmail.com>
2012-09-30 01:14:09 +0400

    Work, bad overlay, slowly

--- perms.py
+++ perms.py
@@ -4,7 +4,7 @@ from  copy import copy
 import numpy
 from scipy.sparse import lil_matrix
 
-N = 2
+N = 3
 
 class TripleRow:
      def __init__(self, first, second, third):
@@ -37,11 +37,12 @@ def generate_op_matrix(triple, two, n):
         matrix[pos] = val
         return True
 
-    matrix = lil_matrix((n,n), dtype=int)
+    matrix = numpy.zeros((n,n), dtype=int)
+    empty_matrix = numpy.zeros((n,n), dtype=int)
     if not set_value(triple.triple[0], triple.triple[1], two[0], matrix):
-        return False
+        return empty_matrix
     if not set_value(triple.triple[1], triple.triple[2], two[1], matrix):
-        return False
+        return empty_matrix
     third_step = (two[0]-1, triple.triple[2]-1)
     forth_step = (triple.triple[0]-1, two[1]-1)
     if matrix[third_step] == matrix[forth_step] and matrix[third_step] != 0:
@@ -55,7 +56,7 @@ def generate_op_matrix(triple, two, n):
     if matrix[third_step] != matrix[forth_step]:
         if matrix[third_step] != 0 and matrix[forth_step] != 0:
             # somehow they are different
-            return False
+            return empty_matrix
         if matrix[third_step] != 0:
             # one of them is not null
             matrix[forth_step] = matrix[third_step]
@@ -63,6 +64,7 @@ def generate_op_matrix(triple, two, n):
             matrix[third_step] = matrix[forth_step]
     return matrix
 
+@profile
 def overlay_well(x_matrix, y_matrix):
     def overlay_of_z(z_inds_set, matrix):
         non_zero_value = 0
@@ -76,7 +78,7 @@ def overlay_well(x_matrix, y_matrix):
                     continue
         return True
 
-    if not x_matrix or not y_matrix:
+    if not x_matrix.any() or not y_matrix.any():
         return False
     x_inds = zip(list(x_matrix.nonzero()[0]), list(x_matrix.nonzero()[1]))
     y_inds = zip(list(y_matrix.nonzero()[0]), list(y_matrix.nonzero()[1]))
@@ -110,12 +112,13 @@ def overlay_well(x_matrix, y_matrix):
             return False
     return True
 
+@profile
 def generate_matrix_table(n, triples):
     matrix_table = numpy.zeros(shape=(n**2, n**3, n**3), dtype=int)
     offset = n**2
     for j_big, triple_big in enumerate(triples):
         for i_big, (two_big, op_matrix_big) in enumerate(triple_big.values):
-            if not op_matrix_big:
+            if not op_matrix_big.any():
                 continue
             for j, triple in enumerate(triples):
                 res = 0
@@ -128,6 +131,50 @@ def generate_matrix_table(n, triples):
     return matrix_table
 
 
+def zero_depth_cells(matrix_table):
+    zero_depth_cells = []
+    for i,cell in enumerate(matrix_table[:,0,:]):
+        if cell.any():
+            zero_depth_cells.append((i,0))
+    return  zero_depth_cells
+
+def next_depth_cells(cell, path, matrix_table):
+    next_depth_cells = []
+    next_depth = cell[1] + 1
+    # get bit columns of all previous steps in path
+    cols = map(lambda cell: matrix_table[cell[0], cell[1], next_depth], path)
+    # overlay them
+    res_map = reduce(lambda x,y: x & y, cols)
+    bits_ar = range(N**2)
+    for i, bit_pos in enumerate(bits_ar):
+        if res_map & 1<<bits_ar[-i-1]:
+            next_depth_cells.append( (bit_pos, next_depth) )
+    return next_depth_cells
+
+all_paths = []
+path     = []
+
+def depth_n(n, matrix_table):
+    global all_paths
+    global path
+    if n >= N**3:
+        all_paths.append(copy(path))
+        return True
+    if n == 0:
+        next_cells = zero_depth_cells(matrix_table)
+    else:
+        cell = path[-1]
+        next_cells = next_depth_cells(cell, path, matrix_table)
+    for new_cell in next_cells:
+        path.append(new_cell)
+        depth_n(n+1, matrix_table)
+        path.pop()
+
+def traverse_table(matrix_table):
+    depth_n(0, matrix_table)
+    return all_paths
+
+
 def main():
     triples = generate_permutations_of_three(N)
     twos = generate_permutations_of_two(N)
@@ -135,9 +182,10 @@ def main():
         for two in twos:
             op_matrix = generate_op_matrix(triple, two, N)
             triple.values.append( (two,op_matrix) )
-#    for t in triples:
-#        print t.triple, t.values
     matrix_table = generate_matrix_table(N, triples)
+    all_paths = traverse_table(matrix_table)
+    print all_paths
+    print len(all_paths)
 
 if __name__ == "__main__":
     main()
